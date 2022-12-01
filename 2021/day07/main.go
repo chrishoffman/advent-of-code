f884dc8ff9d0ce7ec09449363a1c573847db6c1d
Chris Hoffman <99742+chrishoffman@users.noreply.github.com>
2022-12-01 16:32:55 -0500

    adding d7p1

--- 2021/day07/main.go
+++ 2021/day07/main.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"math"
+	"sort"
+	"strconv"
+	"strings"
 
 	"github.com/chrishoffman/advent-of-code"
 )
@@ -12,15 +17,51 @@ func main() {
 }
 
 func problemOne() {
+	points, _ := parseFile()
+
+	minPoint := points[0]
+	maxPoint := points[len(points)-1]
+
+	prevDistance := totalDistance(points, minPoint)
+	for i := minPoint + 1; i < maxPoint; i++ {
+		dist := totalDistance(points, i)
+		if prevDistance < dist {
+			break
+		}
+		prevDistance = dist
+	}
+	fmt.Println(prevDistance)
 }
 
 func problemTwo() {
 }
 
-func parseFile() map[int]int {
+func totalDistance(points []int, dest int) int {
+	var sum float64
+	for _, p := range points {
+		sum += math.Abs(float64(p) - float64(dest))
+	}
+	return int(sum)
+}
+
+// returns sorted list of points and the sum
+func parseFile() ([]int, int) {
 	raw, err := advent.ParseFile("./input.txt")
 	if err != nil {
 		log.Fatalln(err)
 	}
 
+	numbersRaw := strings.Split(raw[0], ",")
+	var numbers []int
+	var sum int
+	for _, numberRaw := range numbersRaw {
+		number, err := strconv.Atoi(numberRaw)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		numbers = append(numbers, number)
+		sum += number
+	}
+	sort.Ints(numbers)
+	return numbers, sum
 }
